c531f36f218adaffe96c3dcb135ae9b10063b481
sanya3024 <72503529+sanya3024@users.noreply.github.com>
2020-10-29 11:23:31 +0530

    uploaded

---
+++ box.js
@@ -0,0 +1,22 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'friction':1,
+          'density': 10,
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
+  
